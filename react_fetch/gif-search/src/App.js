854649fa3327d008e081803c787ece2fb4a4bcb9
MWest2020 <mwesterweel@hotmail.com>
2020-12-29 12:35:12 +0100

    after holiday continuation!

--- react_fetch/gif-search/src/App.js
+++ react_fetch/gif-search/src/App.js
@@ -7,9 +7,25 @@ export default class App extends Component {
   
   constructor() {
     super();
+      this.state = {
+        gif: []
+      };
   } 
+  QGx0HYKQgMVMrdvG8nly3aQfAAVUi7q7
+  //add this function when component is added to the DOM 
+  componentDidMount() {
+    fetch('https://api.giphy.com/v1/gifs/trending?api_key=QGx0HYKQgMVMrdvG8nly3aQfAAVUi7q7&limit=25&rating=G')
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState( { gifs: responseData.data});
+      })
+      .catch((error) => {
+      console.log('Error fetching and passing data', error)
+      });
+  }
 
   render() { 
+    console.log(this.state.logs)
     return (
       <div>
         <div className="main-header">
